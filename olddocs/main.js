924933978627fe2fde7a62e280b20b0699bf1fb7
KAT Binene <asaphbinene@outlook.com>
2021-03-17 20:42:12 +0200

    My working google map

--- olddocs/main.js
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./bases/project-input.js";
-import { ProjectList } from "./bases/project-list.js";
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
-//# sourceMappingURL=main.js.map
